40d7318cac56cb87d67441d0aad49af722baa10d
Tosin Omotayo <oluwaetosin@gmail.com>
2024-10-06 06:46:30 +0100

    add initial migration

---
+++ src/Library.Infrastructure/Migrations/20241006054534_setup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class setup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    ISBN = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Publisher = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    PublicationYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Language = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Edition = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Pages = table.Column<int>(type: "INTEGER", nullable: false),
+                    CopiesAvailable = table.Column<int>(type: "INTEGER", nullable: false),
+                    Location = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
